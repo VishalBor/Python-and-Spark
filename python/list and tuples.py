aee3d3932c3d60e4cb3acd553428dc9d9a8ccb27
VishalBor <122140941+VishalBor@users.noreply.github.com>
2023-01-16 17:07:27 +0530

    initial commit

--- python/list and tuples.py
+++ python/list and tuples.py
@@ -1,2 +1,22 @@
+# list
+# create a list usint using []
+
 a=[1,2,3,4,5,6,6,77,7,8]
+
+# access using index and slicing
 print(a[0])
+print(a[1])
+print(a[0:5])
+print(a[::-1])
+
+# changing the index of list
+a[0]=5
+print(a)
+
+# we can create a list items of diffrent types
+c=[45,"vishal",True,False]
+
+
+# slicing
+friends=["vishal","pooja","akash","ajay","amol"]
+print(friends[0:3])
