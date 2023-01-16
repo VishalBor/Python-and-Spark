f47687f4cbd09ba63e3c680b39f644b63b2b95ff
VishalBor <122140941+VishalBor@users.noreply.github.com>
2023-01-16 15:34:11 +0530

    initial commit

---
+++ python/string functions.py
@@ -0,0 +1,24 @@
+story="once upon a time there was a yutuber named harry who uploaded python course with notes"
+print(len(story))
+print(story.endswith("notes"))
+print(story.capitalize())
+print(story.count("o"))
+print(story.find("once"))
+print(story.title())
+print(story.upper())
+print(story.lower())
+print(story.startswith("once"))
+print(story.index("was"))
+print(story.isupper())
+print(story.replace("harry","vishal"))
+print(story.isalnum())
+print(story.isalpha())
+
+
+# write  a program to find double spaces in string and replace it by single spaces
+a="this is a string with a double  spaces"
+doublespaces=a.find("  ")
+replace=a.replace("  "," ")
+print(doublespaces)
+print(replace)
+
