afb137a864f3a58620a90c793c73590c2ec37ce1
VishalBor <122140941+VishalBor@users.noreply.github.com>
2023-01-16 17:23:04 +0530

    initial commit

--- python/list and tuples.py
+++ python/list and tuples.py
@@ -24,5 +24,28 @@ print(friends[0:3])
 
 # List Methods
 l1=[2,1,2,3,4,5,6,7,7,8,8,9,9,9]
+
+# sort
 l1.sort()
+print(l1)
+
+# reverse
+l1.reverse()
+print(l1)
+
+# appernd
+l1.append(45)
+print(l1)
+
+# insert
+l1.insert(0,0)
+l1.insert(3,5)
+print(l1)
+
+# pop   removes last element
+# l1.pop()
+# print(l1)
+
+# remove  .removes selected element from list
+l1.remove(1)
 print(l1)
